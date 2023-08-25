b2abed918dca78c33079879ecea78e9e45954914
shubham kedia <subham.kedia@oriserve.com>
2023-08-26 00:24:35 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 console.log(process.env.NODE_ENV)
 
 app.get("/", (req, res) => {
-    res.send("WELCOME TO HELL" + process.env.NODE_ENV)
+    res.send(`WELCOME TO HELL ${process.env.NODE_ENV} !!!`)
 })
 
 module.exports = app
