37ac20fc4bb42f441e4d7f8955fddbe2e4a3f85f
shubham kedia <subham.kedia@oriserve.com>
2023-08-26 01:01:31 +0530

    changes temp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const { Task } = require("./models/taskModel")
-const connect = require("./db/mongoose.js")
+// const { Task } = require("./models/taskModel.js")
+// const connect = require("./db/mongoose.js")
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -11,25 +11,25 @@ app.get("/", (req, res) => {
     res.send("Welcome to Notes App")
 })
 
-app.get("/tasks", (req, res) => {
-  Task.find()
-    .then((tasks) => {
-      res.send(tasks)
-    })
-    .catch((err) => {
-      res.status(500).send("Internal Server Error")
-    })
-})
+// app.get("/tasks", (req, res) => {
+//   Task.find()
+//     .then((tasks) => {
+//       res.send(tasks)
+//     })
+//     .catch((err) => {
+//       res.status(500).send("Internal Server Error")
+//     })
+// })
 
-app.get("/tasks/:id", (req, res) => {
-  const id = req.params.id
-  Task.findById(id).then((task) => {
-    if (!task) {
-      return res.status(404).send()
-    }
-    res.send(task)
-  })
-})
+// app.get("/tasks/:id", (req, res) => {
+//   const id = req.params.id
+//   Task.findById(id).then((task) => {
+//     if (!task) {
+//       return res.status(404).send()
+//     }
+//     res.send(task)
+//   })
+// })
 
 module.exports = app
 
