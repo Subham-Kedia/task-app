ea397fb09ea6c23c79f64f0963b431735f30c639
shubham kedia <subham.kedia@oriserve.com>
2023-08-26 01:05:39 +0530

    test

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-require("dotenv").config()
+// require("dotenv").config()
 
-const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
+// const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
 
 const connect = async () => {
-  try {
-    console.log(connectionURL)
-    await mongoose.connect(connectionURL, { dbName: "test" })
-    console.log("mongoose connected")
-  } catch (err) {
-    console.log(err.message)
-  }
+  console.log("hello")
+  // try {
+  //   console.log(connectionURL)
+  //   await mongoose.connect(connectionURL, { dbName: "test" })
+  //   console.log("mongoose connected")
+  // } catch (err) {
+  //   console.log(err.message)
+  // }
 }
 
 module.exports = connect
