99ac2951831bdd050573a06d086936cb91ead4f2
shubham kedia <subham.kedia@oriserve.com>
2023-08-26 01:29:30 +0530

    test1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 require("dotenv").config()
 
 const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
 
-const connect = async () => {
+const connect = () => {
   const str = "HOST is" + abcd
   return str
   // try {
