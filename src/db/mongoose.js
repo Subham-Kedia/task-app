c22e2b4bc71ba5ac7523c66715ae978affe9563e
shubham kedia <subham.kedia@oriserve.com>
2023-08-30 21:24:02 +0530

    changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,7 @@ const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.M
 
 const connect = async () => {
   try {
-    await mongoose.connect(connectionURL, { dbName: "test" })
+    await mongoose.connect(connectionURL, { dbName: "taskManager" })
     console.log("mongoose connected successfully")
   } catch (err) {
     console.log("ERROR-mongoose_connection:", err)
