a2d4e6eb0b8504972fddfb01eedcc370798e1416
shubham kedia <subham.kedia@oriserve.com>
2023-08-30 20:35:04 +0530

    url uncommented

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require("express")
 
 require("dotenv").config()
 
-// const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
+const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
 
 // const connect = async () => {
 //   try {
