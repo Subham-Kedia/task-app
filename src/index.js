57dbaafa209ee179a3f54cb911b3156629bbc43a
shubham kedia <subham.kedia@oriserve.com>
2023-08-30 18:22:02 +0530

    mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require("express")
-require("./db/mongoose")
+// require("./db/mongoose")
 const routers = require("./routers")
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+
+const connectionURL = `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@cluster0.ei6okkf.mongodb.net/?retryWrites=true&w=majority`
+
+const connect = async () => {
+  try {
+    await mongoose.connect(connectionURL, { dbName: "test" })
+    console.log("mongoose connected successfully")
+  } catch (err) {
+    console.log("ERROR-mongoose_connection:", err)
+  }
+}
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(routers)
+
+connect()
 
 module.exports = app
 
