667d83afae231a775f7f447c7abde333d26ec46c
shubham kedia <subham.kedia@oriserve.com>
2023-08-30 20:36:48 +0530

    package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 // require("./db/mongoose")
 // const routers = require("./routers")
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 require("dotenv").config()
 
